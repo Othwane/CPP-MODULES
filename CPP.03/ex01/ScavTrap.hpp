4aacfd43af8f8dda84caae6ea3a40c552771c43f
Othwane <othmanemajdoub8@gmail.com>
2025-06-01 17:45:30 +0100

    ScavTrap Done

---
+++ CPP.03/ex01/ScavTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unky0 <unky0@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 12:33:10 by omajdoub          #+#    #+#             */
+/*   Updated: 2025/06/01 17:39:58 by unky0            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+public:
+    ScavTrap();
+    ScavTrap(std::string name);
+    ScavTrap(const ScavTrap& other);
+    ~ScavTrap();
+    
+    ScavTrap& operator=(const ScavTrap& other);
+    
+    void attack(const std::string& target);
+    void guardGate();
+};
+
+#endif
---
+++ CPP.03/ex01/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: unky0 <unky0@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/07 12:33:10 by omajdoub          #+#    #+#             */
+/*   Updated: 2025/06/01 17:42:13 by unky0            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include <iostream>
+
+int main() {
+    ClapTrap clap1("CL4P-TP");
+    clap1.attack("Bandit");
+    clap1.takeDamage(5);
+    clap1.beRepaired(3);
+    
+    ScavTrap scav1("SC4V-TP");
+    scav1.attack("Raider");
+    scav1.takeDamage(30);
+    scav1.beRepaired(10);
+    scav1.guardGate();
+    
+    return 0;
+}
